f007b1588c4ad0e4c5ddaee34d50a945884f4569
Maciej Witkowiak <mwitkowiak@gmail.com>
2025-05-01 19:26:55 +0200

    loader works, only with blank screen (fast clock)

--- src/burstcart.asm
+++ src/burstcart.asm
@@ -2,7 +2,9 @@
 
 ;--------------------------------------------------
 ; Commodore 16 and Plus/4 Burst cartridge 
-;
+; burst loader based on Pasi Ojala's code
+
+; (c) 2025 by Maciej 'YTM/Elysium' Witkowiak
 
 RAM_ZPVEC1	= $03	; (2) temp
 
@@ -35,9 +37,14 @@ ROM_CHKOUT	= $FFC9
 ROM_CLRCHN	= $FFCC
 ROM_CHROUT	= $FFD2
 
+eFF06		= $FF06
+
 ciabase		= $FD90
 
-; ?detect if CIA is present at CIABASE
+CMD_CHANNEL = 239 ; command channel for burst command
+
+; ?detect if 1551 as #9 or #8 first (ROM sets flag?)
+; +detect if CIA is present at CIABASE
 ; install LOAD wedge
 ; ?detect which drive has burst
 ; ?1551warp for 1551?
@@ -86,11 +93,21 @@ warmstart:
 	lda #>myloadlow
 	sta RAM_ILOAD+1
 
+        ; setup TOD
+	lda #$80                       ; TOD 50Hz, serial IN, timer A stop
+	sta ciabase+14
+	lda #0
+	sta ciabase+11                 ; TOD h
+	sta ciabase+10
+	sta ciabase+9
+	sta ciabase+8
+	lda ciabase+8                  ; load 10ths to start clock
+
 	; welcome message
 +	ldx #0
 -	lda startup_txt,x
 	beq +
-	jsr $ffd2
+	jsr ROM_CHROUT
 	inx
 	bne -
 +	rts
@@ -124,6 +141,8 @@ myloadlow:
 +	lda RAM_VERFCK		; stored A
 loadrom:
 	jmp $F04C		; -> F04C
+bufFF06:
+        !byte 0
 	} ; pseudopc
 
 lowmem_trampoline_end:
@@ -156,22 +175,33 @@ myload:
 
 	; our loading code
 myload_cont:
-	ldy #1
+	ldy #4
 	sty ciabase+4		; set clock rate to the fastest possible
-	dey
+	ldy #0
 	sty ciabase+5
 	lda #$c1		; start timer A, serial OUT, TOD 50Hz
 	sta ciabase+14
 	bit ciabase+13		; clear interrupt register
 	lda #8			; data to be sent and interrupt mask (following BIT)
 	sta ciabase+12		; (we wake up the other end so that it believes we can do burst transfers, actual data doesnt matter)
--	bit ciabase+13		; (but 8 is special because it's a mask for this bit instruction)
-	beq -			; wait until data sent XXX this will lockup if there is no CIA --> add timeout via Y register
 
-	lda #$81
+-       iny
+	bmi NotCIA              ; timeout, CIA not present or not working
+	bit ciabase+13		; (but A=8 is special because it's a mask for this bit instruction)
+	bne CIAFound
+	beq -			; wait until data sent
+
+NotCIA:
+        inc load_status
+	lda #<cia_not_present
+	ldy #>cia_not_present
+	jmp print_msg
+
+CIAFound:
+        lda #$81
 	sta ciabase+14		; start timer A, serial IN, TOD 50Hz
 
-	jsr eF160		;print "SEARCHING"
+	jsr eF160		;print "SEARCHING" ; XXX too early - will show "SEARCHING" twice if device is not burst capable
 
 	lda RAM_FNLEN		; preserve the filename length
 	pha
@@ -182,22 +212,21 @@ myload_cont:
 	sta RAM_FNLEN		; no filename for command channel
 	lda #15
 	sta RAM_SA		; secondary address 15 == command channel
-	lda #239
+	lda #CMD_CHANNEL
 	sta RAM_LA		; logical file number (15 might be in use)
 	jsr ROM_OPEN
 	sta ErrNo+1
 	pla
 	sta RAM_FNLEN		; restore filename length
-	bcs ErrNo		; "device not present", "too many open files", "file already open"
+;	bcs ErrNo		; "device not present", "too many open files", "file already open"
+        bcc +
+	jmp ErrNo
++
 	; Send burst command for Fastload
-	ldx #239
+	ldx #CMD_CHANNEL
 	jsr ROM_CHKOUT		; command channel as output
 	sta ErrNo+1
 	bcs NoDev		; "device not present" or other errors
-
-	lda #8
-	bit ciabase+13		; we should receive something by now
-	beq NotFast
 
 	ldy #3
 -	lda BCMD-1,y
@@ -213,16 +242,23 @@ myload_cont:
 	bne -
 	jsr ROM_CLRCHN		; clear channels	
 
-	sei
+	lda #8			; how C128 detects burst?
+	bit ciabase+13		; we should receive something by now
+	bne +
+	jmp NotFast		; device doesn't handle burst
++
+	jsr eF189		; print LOADING, uses CHROUT will CLI again
+	sei			; loader starts here
+	lda eFF06
+	sta bufFF06		; blank screen
+	lda #0
+	sta eFF06
 	jsr eE2B8		; serial clock on == clk line low
 	bit ciabase+13		; clear interrupt register
 	jsr ToggleClk		; toggle clock
 
 	jsr HandleStat		; get initial status
 	pha			; keep it
-
-	jsr eF189		; print LOADING, uses CHROUT will CLI again
-	sei
 
 	jsr GetByte		; Get the load address (low) - We assume
 				; that every file is at least 2 bytes long
@@ -243,7 +279,7 @@ Loop:	jsr GetAndStore		; Get X bytes and save them
 Last:	tax			; Otherwise it is bytes left. Do the last..
 	jsr GetAndStore		; Get X number of bytes and save them
 	jsr eE2B8		; Serial clock on (the normal value)
-	lda #239
+	lda #CMD_CHANNEL
 	jsr ROM_CLOSE		; Close the command channel
 	clc			; carry clear -> no error indicator
 	bcc End
@@ -254,22 +290,26 @@ FileNotFound:
 	jsr eE2B8		; Serial clock on (the normal value)
 	lda #4			; File not found
 	sta ErrNo+1
-NoDev:	lda #239
+NoDev:	lda #CMD_CHANNEL
 	jsr ROM_CLOSE		; Close the command channel
 ErrNo:	lda #5			; Device not present
 	sec			; carry set -> error indicator
-End:	ldx RAM_MEMUSS		; Loader returns the end address,
+End:	ldx bufFF06
+        stx eFF06
+        ldx RAM_MEMUSS		; Loader returns the end address,
 	ldy RAM_MEMUSS+1	;  so get it into regs..
 	cli
 	rts			; Return from the loader
 				; load_status = 0 so do nothing more
 
 NotFast:			; device doesn't handle burst
-	lda #239
+	lda #CMD_CHANNEL
 	jsr ROM_CLOSE
 	jsr ROM_CLRCHN		; close file
 	inc load_status		; return and pass to ROM load
-	rts
+	lda #<not_burst
+	ldy #>not_burst
+	jmp print_msg
 
 HandleStat:
 	jsr GetByte		; Get a byte (and toggle clk to start the
@@ -287,7 +327,7 @@ HandleStat:
 
 GetAndStore:
 	jsr GetByte		; Get a byte & toggle clk
-sta $ff19 ; XXX flash border
+inc $ff19 ; XXX flash border
 	ldy #0
 	sta (RAM_MEMUSS),y	; Store the byte
 	inc RAM_MEMUSS
@@ -299,13 +339,16 @@ sta $ff19 ; XXX flash border
 
 GetByte:
 	lda #8
--	bit ciabase+13		; wait for a byte
+-
+	bit ciabase+13		; wait for a byte
 	beq -
 ToggleClk:
+        ldy ciabase+12          ; get the latched byte from serial port
 	lda $01
 	eor #%00000010		; toggle the old serial clock (send Ack)
 	sta $01			; disk drive will start sending the next byte
-	lda ciabase+12		; get the latched byte from serial port
+	;lda ciabase+12		; get the latched byte from serial port
+        tya
 	rts
 
 BCMD:	!byte $1f, $30, $55	; 'U0',$1F == Burst Fastload command
@@ -317,6 +360,25 @@ startup_txt:
 	!text " VEC INSTALLED",13,0
 
 	; anything above $C000 comes from KERNAL (see coldstart)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+print_msg:
+
+                sta RAM_ZPVEC1
+		sty RAM_ZPVEC1+1
+		ldy #0
+-               lda (RAM_ZPVEC1),y
+                beq +
+		jsr ROM_CHROUT
+		iny
+		bne -
++               rts
+
+cia_not_present:
+                !text "CIA NOT PRESENT",13,0
+not_burst:
+		!text 13,"NOT BURST CAPABLE",13,0
 
 !fill ($10000-*), $ff
 
